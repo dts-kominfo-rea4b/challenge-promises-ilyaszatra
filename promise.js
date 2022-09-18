c6ef9a6dab9da4f9f134bc1e9f668107d691d9a4
ilyaszatra <muhamadilyassaputra@gmail.com>
2022-09-18 16:57:31 +0700

    melengkapi promise

--- promise.js
+++ promise.js
@@ -1,7 +1,20 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (feels) =>{
+  const theaterIXX = await promiseTheaterIXX();
+  const theaterVGC = await promiseTheaterVGC();
+
+  const concatJson = theaterIXX.concat(theaterVGC);
+  let feelsTotal = 0;
+
+  for (i = 0; i < concatJson.length; i++) {
+    if (concatJson[i].result == feels) {
+      feelsTotal += 1;
+    }
+  }
+  return feelsTotal;
+};
 
 module.exports = {
   promiseOutput,
